cec4b67c772547114415ce44a50eedd401614ca6
rabiul3000 <mdrabiul.asia@gmail.com>
2025-07-23 10:11:58 +0600

    fix some typo

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL,
+    ],
     credentials: true,
   })
 );
